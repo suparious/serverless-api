0c088d6e29d13618ed362808f41d652d988142e7
Shaun Prince <suparious@techfusion.ca>
2017-01-28 12:05:42 -0800

    Configured basic REST endpoint

--- handler.js
+++ handler.js
@@ -1,16 +1,31 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+module.exports.urlcheck = (event, context, callback) => {
+  let dynamicHtml = '<p>Unlisted URL</p>';
+  // check for GET params and use if available
+  if (event.queryStringParameters && event.queryStringParameters.name) {
+    dynamicHtml = `<p>Match found on ${event.queryStringParameters.name}!</p>`;
+  }
+
+  const html = `
+  <html>
+    <style>
+      h1 { color: #73757d; }
+    </style>
+    <body>
+      <h1>URL Check</h1>
+      ${dynamicHtml}
+    </body>
+  </html>`;
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: html,
   };
 
+  // callback is sending HTML back
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
