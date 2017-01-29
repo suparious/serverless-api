9b3c829d08d3b732222f6de1ca4c5de89e859a34
Shaun Prince <suparious@techfusion.ca>
2017-01-29 02:26:55 -0800

    moved to a single endpoint, added more unit tests

--- handler.js
+++ handler.js
@@ -1,19 +1,11 @@
 'use strict';
 
-module.exports.hostInfo = (event, context, callback) => {
-  const hostInfo = event.pathParameters.hostname_and_port
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ "message": 'Validating ' + hostInfo + '!'})
-  };
-  callback(null, response);
-};
-
-module.exports.queryInfo = (event, context, callback) => {
+module.exports.urlInfo = (event, context, callback) => {
+  const hostInfo  = event.pathParameters.hostname_and_port
   const queryInfo = event.pathParameters.original_path_and_query_string
-  const response = {
+  const response  = {
     statusCode: 200,
-    body: JSON.stringify({ "message": 'Validating ' + queryInfo + '!'})
+    body: JSON.stringify({ "message": 'Validating ' + hostInfo + queryInfo + '!'})
   };
   callback(null, response);
 };
