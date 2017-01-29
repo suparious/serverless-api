e3a52f202551656f50d2bccab31a418bd94025aa
Shaun Prince <suparious@techfusion.ca>
2017-01-29 11:31:22 -0800

    added constrints on input completion

--- handler.js
+++ handler.js
@@ -4,9 +4,17 @@ module.exports.urlInfo = (event, context, callback) => {
   const hostInfo  = event.pathParameters.hostname_and_port;
   const queryInfo = event.pathParameters.original_path_and_query_string;
 
+  // ToDo: Add some actual validation code here
+  const status = 'FAIL';
+  if (hostInfo == null) {
+    let status = 'isnull';
+  } else {
+    let status = 'notnull';
+  }
+
   const response  = {
           statusCode: 200,
-          body: JSON.stringify({ "message": 'Validated: ' + hostInfo + '/' + queryInfo + '!'})
+          body: JSON.stringify({ "message": status + ':' + hostInfo + '/' + queryInfo + '!'})
   };
   callback(null, response);
-}
+};
