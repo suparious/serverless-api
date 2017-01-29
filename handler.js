69315179b6abd951b1b9dd1bba34d14672ca770f
Shaun Prince <suparious@techfusion.ca>
2017-01-29 01:08:42 -0800

    split out host and query to use separate handlers

--- handler.js
+++ handler.js
@@ -1,11 +1,10 @@
 'use strict';
 
-module.exports.urlinfo = (event, context, callback) => {
+module.exports.hostinfo = (event, context, callback) => {
   let hostInfo    = `${event.queryStringParameters.hostinfo}`;
-  let queryInfo   = `${event.queryStringParameters.queryinfo}`;
-  let dynamicHtml = '<p>URL not found</p><p>usage: GET /urlinfo/1/{hostname_and_port}/{original_path_and_query_string}</p>';
+  let dynamicHtml = '<p>Incomplete URL</p><p>usage: GET /urlinfo/1/{hostname_and_port}/{original_path_and_query_string}</p>';
   // check for GET params and use if available
-  if (event.queryStringParameters && event.queryStringParameters.hostinfo && event.queryStringParameters.queryinfo) {
+  if (event.queryStringParameters && event.queryStringParameters.hostinfo) {
     dynamicHtml = `<p>Looking up request for: ${event.queryStringParameters.hostinfo}!</p>`;
   }
 
@@ -15,10 +14,41 @@ module.exports.urlinfo = (event, context, callback) => {
       h1 { color: #73757d; }
     </style>
     <body>
-      <h1>URL Security Check</h1>
+      <h1>Domain Check</h1>
       ${dynamicHtml}
-      Host information: ${hostInfo}
-      Path and query: ${queryInfo}
+      Diagnostics: ${hostInfo}
+    </body>
+  </html>`;
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: html,
+  };
+
+  // callback is sending HTML back
+  callback(null, response);
+};
+
+module.exports.queryinfo = (event, context, callback) => {
+  let queryInfo   = `${event.queryStringParameters.queryinfo}`;
+  let dynamicHtml = '<p>Query not found</p>';
+  // check for GET params and use if available
+  if (event.queryStringParameters && event.queryStringParameters.queryinfo) {
+    dynamicHtml = `<p>Validating query: ${event.queryStringParameters.queryinfo}!</p>`;
+  }
+
+  const html = `
+  <html>
+    <style>
+      h1 { color: #73757d; }
+    </style>
+    <body>
+      <h1>Path and Query check</h1>
+      ${dynamicHtml}
+      Diagnostics: ${queryInfo}
     </body>
   </html>`;
 
