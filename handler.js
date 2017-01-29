8325ebb5e042165da1340cd70d5e1f1e34a922d5
Shaun Prince <suparious@techfusion.ca>
2017-01-29 10:56:14 -0800

    removed validation component, to restruture the handler. Resolved Lamda errors. Working... yaay!

--- handler.js
+++ handler.js
@@ -3,18 +3,10 @@
 module.exports.urlInfo = (event, context, callback) => {
   const hostInfo  = event.pathParameters.hostname_and_port;
   const queryInfo = event.pathParameters.original_path_and_query_string;
-  const validation = {
-    if (hostInfo && queryInfo) {
-      const response  = {
+
+  const response  = {
           statusCode: 200,
           body: JSON.stringify({ "message": 'Validated: ' + hostInfo + '/' + queryInfo + '!'})
-      };
-    } else {
-      const response  = {
-          statusCode: 403,
-          body: JSON.stringify({ "message": 'Not-Valid: ' + hostInfo + '/' + queryInfo + '!'})
-      };
-    };
   };
   callback(null, response);
 }
