913cbf6547c64ae950e988b4aa1e8ad817e06d77
Shaun Prince <suparious@techfusion.ca>
2017-01-29 03:37:43 -0800

    lamda function is broken now

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ module.exports.urlInfo = (event, context, callback) => {
   const hostInfo  = event.pathParameters.hostname_and_port;
   const queryInfo = event.pathParameters.original_path_and_query_string;
   const validation = {
-    if (hostInfo) {
+    if (hostInfo && queryInfo) {
       const response  = {
           statusCode: 200,
           body: JSON.stringify({ "message": 'Validated: ' + hostInfo + '/' + queryInfo + '!'})
@@ -14,6 +14,7 @@ module.exports.urlInfo = (event, context, callback) => {
           statusCode: 403,
           body: JSON.stringify({ "message": 'Not-Valid: ' + hostInfo + '/' + queryInfo + '!'})
       };
+    };
   };
   callback(null, response);
 }
