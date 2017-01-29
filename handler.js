7e78eed57a587372b9cb269eb59cf2ea7241401f
Shaun Prince <suparious@techfusion.ca>
2017-01-28 23:49:16 -0800

    added some debugging outputs

--- handler.js
+++ handler.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports.urlinfo = (event, context, callback) => {
-  let dynamicHtml = '<p>URL not found<br>usage: GET /urlinfo/1/{hostname_and_port}/{original_path_and_query_string}</p>';
+  let dynamicHtml = '<p>URL not found</p><p>usage: GET /urlinfo/1/{hostname_and_port}/{original_path_and_query_string}</p><p>Diagnostics<br>hostname_and_port: ${event.queryStringParameters.hostinfo}</p>';
   // check for GET params and use if available
-  if (event.queryStringParameters) {
+  if (event.queryStringParameters && event.queryStringParameters.hostinfo && event.queryStringParameters.queryinfo) {
     dynamicHtml = `<p>Looking up: ${event.queryStringParameters.hostinfo}!<br>using the following query: ${event.queryStringParameters.queryinfo}</p>`;
   }
 
@@ -15,6 +15,8 @@ module.exports.urlinfo = (event, context, callback) => {
     <body>
       <h1>URL Security Check</h1>
       ${dynamicHtml}
+      Host information: ${event.queryStringParameters.hostinfo}
+      Path and query: ${event.queryStringParameters.queryinfo}
     </body>
   </html>`;
 
