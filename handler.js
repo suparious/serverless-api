7988e327b85813e592b5423259a276f4a052dd56
Shaun Prince <suparious@techfusion.ca>
2017-01-30 16:32:30 -0800

    reformatted the output, added input validation.

--- handler.js
+++ handler.js
@@ -5,16 +5,42 @@ module.exports.urlInfo = (event, context, callback) => {
   const queryInfo = event.pathParameters.original_path_and_query_string;
 
   // ToDo: Add some actual validation code here
-  let status = 'FAIL';
-  if (typeof hostInfo === 'undefined' || hostInfo === null) {
-    let status = 'INCOMPLETE';
-  } else {
-    let status = 'PASS';
+  //if (event.pathParameters.hostname_and_port === 'undefined' || event.pathParameters.hostname_and_port === null) {
+  //  let status = 'INVALID';
+  //} else {
+  //  let status = 'PASS';
+  //}
+
+  // Debugging
+  let status = 'undefined';
+  let result = 'undefined';
+
+  // Validate the input
+  if (hostInfo && queryInfo) {
+    status = 'RECIEVED'
   }
+
+  // process the request
+  if (status = 'RECIEVED') {
+    // check against a DunamoDB table
+    status = 'Successfully processed your request';
+    result = 'PASS';
+  } else {
+    status = 'Successfully processed your request';
+    result = 'FAIL';
+  }
+
 
   const response  = {
           statusCode: 200,
-          body: JSON.stringify({ "message": status + ':' + hostInfo + '/' + queryInfo + '!'})
+          headers: {
+            "x-custom-header" : "urlInfo - results"
+          },
+          body: JSON.stringify({
+            request:' ' + hostInfo + '/' + queryInfo,
+            status:' ' + status,
+            result:' ' + result
+          }, null, '\t')
   };
   callback(null, response);
 };
